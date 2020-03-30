5970e777ac064e601b6bbe068fef976305444016
LISIČIČ <ml3551@student.uni-lj.si>
2020-03-31 01:51:15 +0200

    - adding treeChopper.lua program placeholder

--- init.lua
+++ init.lua
@@ -10,6 +10,7 @@
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/globalVariables.lua", "globalVariables.cfg")
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/programs/mine.lua", "programs/mine.lua")
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/programs/ref.lua", "programs/ref.lua")
+shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/programs/treeChopper.lua", "programs/treeChopper.lua")
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/apis/computerAPI.lua", "apis/computerAPI.lua")
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/apis/fv.lua", "apis/fv.lua")
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/apis/turtleAPI.lua", "apis/turtleAPI.lua")
