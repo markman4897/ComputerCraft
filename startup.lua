d334eb622a7858c637e97d9fd3c18da5a4f16399
LISIČIČ <ml3551@student.uni-lj.si>
2020-03-30 20:49:47 +0200

    fix

--- startup.lua
+++ startup.lua
@@ -5,4 +5,6 @@
 
 -- get
 shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/startup.lua", "sartup.lua")
-("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/install.luashell.run", "install.lua")
+shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/install.luashell.run", "install.lua")
+shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/programs/ref.lua", "programs/ref.lua")
+shell.run("programs/ref.lua")
