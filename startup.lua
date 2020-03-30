c4ba9d9339283399c18c8d5798883d864e3010de
LISIČIČ <ml3551@student.uni-lj.si>
2020-03-30 20:46:47 +0200

    starting on startup.lua file

--- startup.lua
+++ startup.lua
@@ -2,3 +2,7 @@
 
 -- Startup script to make (download) environment on the first run and to
 -- properly run every consecutive time
+
+-- get
+shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/startup.lua", "sartup.lua")
+("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/install.luashell.run", "install.lua")
