1cec71253279707fa1cd6a12d5d542f625809597
LISIČIČ <ml3551@student.uni-lj.si>
2020-03-30 20:21:34 +0200

    refuel script and folder restructure

---
+++ programs/ref.lua
@@ -0,0 +1,13 @@
+--CC
+
+-- program to quickly refuel from first four slots
+
+turtle.select(1)
+turtle.refuel()
+turtle.select(2)
+turtle.refuel()
+turtle.select(3)
+turtle.refuel()
+turtle.select(4)
+turtle.refuel()
+turtle.select(1)
--- startup.lua
+++ startup.lua
@@ -1,1 +1,4 @@
 --CC
+
+-- Startup script to make (download) environment on the first run and to
+-- properly run every consecutive time
