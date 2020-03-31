02e28b32bb2cbe3e8f874b616c8b9f6308952533
LISIČIČ <ml3551@student.uni-lj.si>
2020-03-31 02:07:05 +0200

    - minor fixes

--- startup.lua
+++ startup.lua
@@ -12,11 +12,11 @@ local tArgs = {...}
 -- found.
 
 local function uninstall()
-    files = {"startup",
-             "resume",
-             "globalVariables.cfg",
-             "apis",
-             "programs"}
+    files = {"/startup",
+             "/globalVariables.cfg",
+             "/apis", -- folder
+             "/programs", -- folder
+             "/resume"}
 
     for y,x in pairs(files) do
       -- should this be try() ? or pcall(func, arg) or whatever for extra safety?
@@ -32,12 +32,14 @@ if tArgs[1] == "uninstall" then
   return true
 end
 
+print("[INFO]: Got through uninstall.")
+
 if not fs.exists("globalVariables.cfg") then
   shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/init.lua", "init.lua")
   shell.run("init.lua")
   fs.delete("init.lua")
 else
-  shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/globalVariables.lua", "temp")
+  shell.run("wget", "https://raw.githubusercontent.com/markman4897/ComputerCraft/master/globalVariables.cfg", "temp")
   local new_ver = textutils.unserialise(fs.open("temp", "r").readAll())
   new_ver.close()
   local ver = textutils.unserialise(fs.open("globalVariables.cfg", "r").readAll())
